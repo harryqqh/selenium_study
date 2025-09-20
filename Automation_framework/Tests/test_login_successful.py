f0ddc6660bcd96c87b6b96db4897dee43f30b416
Harry <harry.qqh1995@gmail.com>
2025-09-20 19:47:14 +0700

    Automation_framework/

--- Automation_framework/Tests/test_login_successful.py
+++ Automation_framework/Tests/test_login_successful.py
@@ -5,7 +5,9 @@ from pages.dashboard_page import DasboardPage
 import pytest
 
 class TestLogin(BaseTest):
- 
+    
+        @pytest.mark.smoke # Marking test suites
+        
         def test_login_orange(self):
             print("🔵 Starting login test")
             # Initialize page objects
--- Automation_framework/base/base_test.py
+++ Automation_framework/base/base_test.py
@@ -2,13 +2,16 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options # Import function form library for optional interaction
 
 
 class BaseTest:
     @pytest.fixture(scope="class", autouse=True)
     #Initialize webdriver
     def setup(self, request):
-        driver = webdriver.Chrome()
+        chrome_options = webdriver.ChromeOptions() # definition option 
+        chrome_options.add_argument("--headless") # add option to Run headless (no UI)
+        driver = webdriver.Chrome(options=chrome_options) # implement option 
         driver.get("https://opensource-demo.orangehrmlive.com/")
         driver.maximize_window()
         request.cls.driver = driver
